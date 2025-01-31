38459fae75fe278e43f3e64625b8acd5eb25ad45
dashath0097 <dashathk0440@gmail.com>
2025-01-31 10:37:39 +0530

    Update first.tf

--- first.tf
+++ first.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your preferred AMI
+  ami           = "ami-0c614dee691cbbf37"  # Replace with your preferred AMI
   instance_type = "t2.micro"
 
   tags = {
